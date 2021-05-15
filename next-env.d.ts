f92344397447049ca2a82620e0398418d24ff3ad
Joseph Moshood <gladstargtschool6@gmail.com>
2021-05-15 12:19:26 +0100

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,7 +3,7 @@
 # Update these with your Supabase details from your project settings > API
 NEXT_PUBLIC_SUPABASE_URL=https://imdvvfcbtrfewysxgrnr.supabase.co
 NEXT_PUBLIC_SUPABASE_CLIENT_KEY=3c89de2b-ff7c-47ec-bd52-06317b777ab1
-SUPABASE_SERVICE_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNDMzNzg3MSwiZXhwIjoxOTI5OTEzODcxfQ.O7Ck0RFbXxsJ9BpxdAuey5O_MJcC_lHM06xDYKTbWxg
+SUPABASE_SERVICE_KEY=eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNDMzNzg3MSwiZXhwIjoxOTI5OTEzODcxfQ.O7Ck0RFbXxsJ9BpxdAuey5O_MJcC_lHM06xDYKTbWxg
 
 
 # Update these with your Stripe credentials from https://dashboard.stripe.com/apikeys
